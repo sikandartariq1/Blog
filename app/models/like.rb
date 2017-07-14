e075e15dc358e4a8624887ab68644b4e61561afa
Sikandar Tariq <sikandar.tariq@amblersaleem.com>
2017-07-14 18:25:27 +0500

    Addition of callbacks   New Callbacks    Like => after_create    Like => after_commit

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,14 @@
 class Like < ApplicationRecord
   belongs_to :likeable, :polymorphic => true
-end
 
+  after_create :just_created
+  after_commit :saved
+
+  protected
+  def just_created
+  	puts "just liked..!!"
+  end
+  def saved
+  	puts "Like saved to database"
+  end
+end
